acdcd811f1def49004f5f47e6eb35fe5b006a1c3
root <root@ip-172-31-29-103.ec2.internal>
2025-07-10 19:16:52 +0000

    changes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -d --name guvi-project-react-app -p 80:80 guvi-project-react-app
+# Stop and remove the container if it already exists
+docker rm -f guvi-project-react-app 2>/dev/null || true
+
+# Run the container
+docker run -d --name guvi-project-react-app -p 80:80 srihariops/guvi_reactapp_prod:latest
+
